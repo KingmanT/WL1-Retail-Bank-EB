057398dc64036390bbe35a3ccce2a767ddf5417d
Kingman <107763929+KingmanT@users.noreply.github.com>
2024-07-09 15:18:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -661,4 +661,4 @@ def customerlog():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
